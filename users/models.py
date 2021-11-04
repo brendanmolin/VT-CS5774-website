7189f7444f54248a84d68ad3407ce4e523a198df
BrendanMolin <brendan.molin@vt.edu>
2021-11-04 07:56:19 -0400

    correct profile creation

--- users/models.py
+++ users/models.py
@@ -17,4 +17,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_details(sender, instance, **kwargs):
-    instance.details.save()
+    instance.profile.save()
